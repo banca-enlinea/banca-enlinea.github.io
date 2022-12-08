999c555f73ee203c0ef4d2c4714941d3f561791c
bancanet <118914760+banca-enlinea@users.noreply.github.com>
2022-12-08 11:19:04 -0600

    Update form.js

--- js/form.js
+++ js/form.js
@@ -1,17 +1,17 @@
-(function() {
-var $form = $('form#formulario'), url = 'https://script.google.com/macros/s/AKfycbz2yuWrZGcVypyZ1BD7pAlxAwOYvyaurab249gpZIFNZP2zxL-el4ZBOBkxBbyeO5VtnA/exec';
-$('#submit-contact-form').on('click', function(e) {
-e.preventDefault();
-var jqxhr = $.ajax({
-url: url,
-method: "GET",
-dataType: "json",
-data: $form.serializeObject(),
-success: function(data) {
-console.log(data);
-$('form#formulario').hide();
-$('body').append("<p style='color:green'>Gracias por tu interés. Contactaremos contigo en breve.</p>");
-}
-});
-});
-})();
+ (function() {
+ var $form = $('form#contact-form'), url = 'https://script.google.com/macros/s/AKfycbz2yuWrZGcVypyZ1BD7pAlxAwOYvyaurab249gpZIFNZP2zxL-el4ZBOBkxBbyeO5VtnA/exec';
+ $('#submit-contact-form').on('click', function(e) {
+ e.preventDefault();
+ var jqxhr = $.ajax({
+ url: url,
+ method: "GET",
+ dataType: "json",
+ data: $form.serializeObject(),
+ success: function(data) {
+ console.log(data);
+ $('form#contact-form').hide();
+ $('body').append("<p style='color:green'>Gracias por tu interés. Contactaremos contigo en breve.</p>");
+ }
+ });
+ });
+ })();
